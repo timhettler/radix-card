607c9283db5c99a93f80b7f0a7b57c6466d59fc3
Tim Hettler <hettler@work.co>
2023-01-23 16:59:28 -0500

    fix: story config

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -1,6 +1,6 @@
 const path = require("path");
 module.exports = {
-  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  stories: ["../test/**/*.mdx", "../test/**/*.stories.@(js|jsx|ts|tsx)"],
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
